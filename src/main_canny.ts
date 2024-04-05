354b898c1138eb790762672edbc4bd5726cbd380
Yusuke Kawamoto <novogrammer@gmail.com>
2024-04-05 17:03:42 +0900

    cv2をimportする

--- src/main_canny.ts
+++ src/main_canny.ts
@@ -30,6 +30,7 @@ async function hello_python() {
   });
 
   await pyodide.runPythonAsync(`import numpy as np`);
+  await pyodide.runPythonAsync(`import cv2`);
   await pyodide.runPythonAsync(`x = np.array([1, 2, 3])`);
   return pyodide.runPythonAsync("1+1");
 }
